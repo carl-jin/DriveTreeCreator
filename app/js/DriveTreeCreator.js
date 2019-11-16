94e4119d8853a02420da69f4ca53c72a8d7cbf61
CarlJin <buffpal2016@gmail.com>
2019-11-15 22:10:46 -0500

    Update DriveTreeCreator.js

--- app/js/DriveTreeCreator.js
+++ app/js/DriveTreeCreator.js
@@ -8,7 +8,7 @@
  * --------------------------------------------------------------------------
  */
 class DriveTreeCreator {
-  constructor (options) {
+  constructor(options) {
     //  default options
     this.options = {
       googleAPI: {
@@ -29,10 +29,12 @@ class DriveTreeCreator {
         by google drive api, cuz google not provide it
         but interestingly we can use the owner parameter to replace it!
         */
-        owner: "",
+        owner: ""
       },
-      scope: 'https://www.googleapis.com/auth/drive',
-      discovery_docs: ["https://script.googleapis.com/$discovery/rest?version=v1"],
+      scope: "https://www.googleapis.com/auth/drive",
+      discovery_docs: [
+        "https://script.googleapis.com/$discovery/rest?version=v1"
+      ],
       //  list files except trash file
       inTrash: false,
       // files: `id,title,fileSize,createdDate,webContentLink,embedLink,mimeType,parents,fileExtension,downloadUrl`,
@@ -41,34 +43,35 @@ class DriveTreeCreator {
       includeTeamDriveItems: false,
       //  sort file from dir view, accept a fn
       sort: null
-    }
-    this.options = Object.assign(this.options, options)
-    
+    };
+    this.options = Object.assign(this.options, options);
   }
-  
+
   /**
    * a method from initialization google api environment
    * which mean u should call it after new DriveTreeCreator,immediately
    * and every method calling should after init finish
    * @returns {Promise<any>} when environment has been ready for roll
    */
-  init () {
+  init() {
     return new Promise(async res => {
       if (window.gapi) {
         if (!window.gapi.client.script || !window.gapi.client.drive) {
-          throw new Error('make sure gapi.client.script or gapi.client.drive has been set up, use gapi.load pls');
+          throw new Error(
+            "make sure gapi.client.script or gapi.client.drive has been set up, use gapi.load pls"
+          );
         } else {
-          this._event()
-          res()
+          this._event();
+          res();
         }
       } else {
-        await this._loadGApi()
-        this._event()
-        res()
+        await this._loadGApi();
+        this._event();
+        res();
       }
-    })
+    });
   }
-  
+
   /**
    * subscribe event
    *
@@ -77,37 +80,43 @@ class DriveTreeCreator {
    *
    * @private
    */
-  _event () {
+  _event() {
     //  user sign in state change
-    window.gapi.auth2.getAuthInstance().isSignedIn.listen(isSignedIn => this._emitEvent('signInStateChange', {state: isSignedIn}))
-    
-    this._emitEvent('signInStateChange', {state: this.isSignIn()})
+    window.gapi.auth2
+      .getAuthInstance()
+      .isSignedIn.listen(isSignedIn =>
+        this._emitEvent("signInStateChange", { state: isSignedIn })
+      );
+
+    this._emitEvent("signInStateChange", { state: this.isSignIn() });
   }
-  
+
   /**
    * manually emit an event
    * @private
    */
-  _emitEvent (eventName, payload = {}) {
+  _emitEvent(eventName, payload = {}) {
     if (!this.handlers) {
-      return
+      return;
     }
-    
-    let handlers = this.handlers[eventName]
-    handlers && handlers.forEach(handler => handler(payload))
+
+    let handlers = this.handlers[eventName];
+    handlers && handlers.forEach(handler => handler(payload));
   }
-  
+
   /**
    * get pop page to sign in google account
    */
-  signIn () {
-    return window.gapi.auth2.getAuthInstance().signIn({prompt: 'select_account'})
+  signIn() {
+    return window.gapi.auth2
+      .getAuthInstance()
+      .signIn({ prompt: "select_account" });
   }
-  
-  signOut () {
-    return gapi.auth2.getAuthInstance().signOut()
+
+  signOut() {
+    return gapi.auth2.getAuthInstance().signOut();
   }
-  
+
   /**
    * start to create tree view
    * @returns {Promise<any>}
@@ -117,181 +126,189 @@ class DriveTreeCreator {
    * 3. sort
    * 4. return specific folder from folderId parameter
    */
-  start (cache = false) {
+  start(cache = false) {
     if (!this.isSignIn()) {
-      throw new Error('cant run start method, should sign-in first')
+      throw new Error("cant run start method, should sign-in first");
     }
-    
+
     return new Promise(async res => {
       //  step 1
       //  get all files by owners user
-      this._emitEvent('loadProcess', {process: 'start', count: 0})
-      let files = []
+      this._emitEvent("loadProcess", { process: "start", count: 0 });
+      let files = [];
       if (cache) {
-        let cache = window.localStorage.getItem('filesCache')
-        if (window.localStorage.getItem('filesCache')) {
-          files = JSON.parse(cache)
+        let cache = window.localStorage.getItem("filesCache");
+        if (window.localStorage.getItem("filesCache")) {
+          files = JSON.parse(cache);
         } else {
-          files = await this._getAllFilesUnderRootFolder()
+          files = await this._getAllFilesUnderRootFolder();
           //  data clean
-          files = this._cleanFiles(files)
-          window.localStorage.setItem('filesCache', JSON.stringify(files))
+          files = this._cleanFiles(files);
+          window.localStorage.setItem("filesCache", JSON.stringify(files));
         }
       } else {
-        files = await this._getAllFilesUnderRootFolder()
+        files = await this._getAllFilesUnderRootFolder();
       }
-      
+
       //   step 2
       //  createTree by files
-      let dir = this._createTree(files)
-      
+      let dir = this._createTree(files);
+
       //  step 3
       //  sort
       // if (!~this.options.files.indexOf('name')) {
       //   throw new Error('should include `name` field in options.files')
       // }
-      
+
       Object.keys(dir).map(key => {
-        this._sort(dir[key])
-      })
-      
+        this._sort(dir[key]);
+      });
+
       //  step 4
       //  return specific folder from folderId parameter
-      dir = Object.values(dir)
-      
-      
-      let result = []
-      this.options.googleAPI.folderId.split(',').map(id => result.push(this._returnSpecificFolder(dir, id)))
-      res(result)
-    })
+      dir = Object.values(dir);
+
+      let result = [];
+      this.options.googleAPI.folderId
+        .split(",")
+        .map(id => result.push(this._returnSpecificFolder(dir, id)));
+      res(result);
+    });
   }
-  
+
   /**
    * data clean
    * @param files
    * @private
    */
-  _cleanFiles(files){
-    for(let i = 0;i<files.length;i++){
-      let parents = []
-      files[i].parents.map(item=>parents.push(item.id))
-      
+  _cleanFiles(files) {
+    for (let i = 0; i < files.length; i++) {
+      let parents = [];
+      files[i].parents.map(item => parents.push(item.id));
+
       files[i] = {
-        id:files[i].id,
-        name:files[i].title,
-        size:files[i].fileSize,
-        createdTime:files[i].createdDate,
-        webContentLink:files[i].webContentLink,
-        webViewLink:files[i].embedLink,
-        mimeType:files[i].mimeType,
-        parents:parents,
-        fileExtension:files[i].fileExtension,
-        downloadUrl:files[i].downloadUrl,
-      }
+        id: files[i].id,
+        name: files[i].title,
+        size: files[i].fileSize,
+        createdTime: files[i].createdDate,
+        webContentLink: files[i].webContentLink,
+        webViewLink: files[i].embedLink,
+        mimeType: files[i].mimeType,
+        parents: parents,
+        fileExtension: files[i].fileExtension,
+        downloadUrl: files[i].downloadUrl
+      };
     }
-    return files
+    return files;
   }
-  
+
   /**
    * find specific folder from dir
    * @private
    */
-  _returnSpecificFolder (dir, id) {
-    for (let i = 0 ; i < dir.length ; i++) {
+  _returnSpecificFolder(dir, id) {
+    for (let i = 0; i < dir.length; i++) {
       if (dir[i].id === id) {
-        return dir[i]
+        return dir[i];
       } else {
         if (dir[i].children) {
-          let res = this._returnSpecificFolder(dir[i].children, id)
+          let res = this._returnSpecificFolder(dir[i].children, id);
           if (res) {
-            return res
+            return res;
           }
         }
       }
     }
   }
-  
-  _sort (dir) {
+
+  _sort(dir) {
     if (dir.children && dir.children.length > 0) {
       //  sort
-      dir.children.sort((a, b) => this.options.sort ? this.options.sort(a, b) : a.name.localeCompare(b.name, 'zh'))
-      dir.children.map(item => this._sort(item))
+      dir.children.sort((a, b) =>
+        this.options.sort
+          ? this.options.sort(a, b)
+          : a.name.localeCompare(b.name, "zh")
+      );
+      dir.children.map(item => this._sort(item));
     }
   }
-  
+
   /**
    * core !!!!!!!!!!!!!!!!!!!!!!!!!
    * generator three view by parents from each file
    * @param files
    * @private
    */
-  _createTree (files) {
-    let relationKeyPath = {}
-    let newData = {}
-    
-    files.map(item => newData[item.id] = item)
-    files = newData
-    newData = null
-    
+  _createTree(files) {
+    let relationKeyPath = {};
+    let newData = {};
+
+    files.map(item => (newData[item.id] = item));
+    files = newData;
+    newData = null;
+
     //  core
     Object.keys(files).map(key => {
       if (files[key].parents) {
-        let parentId = files[key].parents[0]
-        let item = files[key]
-        
+        let parentId = files[key].parents[0];
+        let item = files[key];
+
         if (files[parentId]) {
           if (files[parentId].children) {
-            files[parentId].children.push(item)
+            files[parentId].children.push(item);
           } else {
-            files[parentId].children = [item]
+            files[parentId].children = [item];
           }
-          
-          relationKeyPath[key] = [parentId]
-          delete files[key]
+
+          relationKeyPath[key] = [parentId];
+          delete files[key];
         } else {
           if (relationKeyPath[parentId]) {
-            relationKeyPath[parentId] = this.__findKeyPath(parentId, relationKeyPath)
-            let parentObj = null
+            relationKeyPath[parentId] = this.__findKeyPath(
+              parentId,
+              relationKeyPath
+            );
+            let parentObj = null;
             relationKeyPath[parentId].map(_parentId => {
               if (Array.isArray(parentObj)) {
                 parentObj.map((__parentItem, __key) => {
                   if (__parentItem.id === _parentId) {
-                    parentObj = parentObj[__key].children
+                    parentObj = parentObj[__key].children;
                   }
-                })
+                });
               } else {
-                parentObj = files[_parentId].children
+                parentObj = files[_parentId].children;
               }
-            })
-            
+            });
+
             parentObj.map(___item => {
               if (___item.id === parentId) {
                 if (___item.children) {
-                  ___item.children.push(item)
+                  ___item.children.push(item);
                 } else {
-                  ___item.children = [item]
+                  ___item.children = [item];
                 }
               }
             });
-            relationKeyPath[key] = [parentId]
-            delete files[key]
+            relationKeyPath[key] = [parentId];
+            delete files[key];
           }
         }
       }
-    })
-    
-    return files
+    });
+
+    return files;
   }
-  
-  __findKeyPath (key, relationPath, arr = []) {
+
+  __findKeyPath(key, relationPath, arr = []) {
     if (relationPath[key]) {
-      arr = relationPath[key].concat(arr)
-      return this.__findKeyPath(arr[0], relationPath, arr)
+      arr = relationPath[key].concat(arr);
+      return this.__findKeyPath(arr[0], relationPath, arr);
     } else {
-      return arr
+      return arr;
     }
   }
-  
+
   /**
    * get all files by owners user
    * @param nextPageToken
@@ -300,138 +317,160 @@ class DriveTreeCreator {
    * @returns {Promise<any>}
    * @private
    */
-  _getAllFilesUnderRootFolder (nextPageToken = '', data = [], count = 1) {
+  _getAllFilesUnderRootFolder(nextPageToken = "", data = [], count = 1) {
     return new Promise(res => {
-      window.gapi.client.drive.files.list({
-        maxResults: 1000,
-        pageToken: nextPageToken,
-        spaces:'drive',
-        q: `'${this.options.googleAPI.owner}' in owners and trashed = ${this.options.inTrash.toString()}`,
-        // fields: `nextPageToken, files(${this.options.files})`,
-        includeTeamDriveItems: this.options.includeTeamDriveItems
-      }).then(result => {
-        data = data.concat(result.result.items)
-        //  emit event
-        this._emitEvent('loadProcess', {process: count, count: result.result.items.length})
-        count++
-        if (result.result.nextPageToken) {
-          res(this._getAllFilesUnderRootFolder(result.result.nextPageToken, data, count))
-        } else {
-          this._emitEvent('loadProcess', {process: 'down', count: data.length})
-          res(data)
-        }
-      });
+      window.gapi.client.drive.files
+        .list({
+          maxResults: 1000,
+          pageToken: nextPageToken,
+          spaces: "drive",
+          q: `'${
+            this.options.googleAPI.owner
+          }' in owners and trashed = ${this.options.inTrash.toString()}`,
+          // fields: `nextPageToken, files(${this.options.files})`,
+          includeTeamDriveItems: this.options.includeTeamDriveItems
+        })
+        .then(result => {
+          data = data.concat(result.result.items);
+          //  emit event
+          this._emitEvent("loadProcess", {
+            process: count,
+            count: result.result.items.length
+          });
+          count++;
+          if (result.result.nextPageToken) {
+            res(
+              this._getAllFilesUnderRootFolder(
+                result.result.nextPageToken,
+                data,
+                count
+              )
+            );
+          } else {
+            this._emitEvent("loadProcess", {
+              process: "down",
+              count: data.length
+            });
+            res(data);
+          }
+        });
     });
   }
-  
+
   /**
    * check user Sign In state
    */
-  isSignIn () {
-    return window.gapi.auth2.getAuthInstance().isSignedIn.get()
+  isSignIn() {
+    return window.gapi.auth2.getAuthInstance().isSignedIn.get();
   }
-  
+
   /**
    * Attach a handler function for an event.
    * @param eventName
    * @param handler
    */
-  on (eventName, handler) {
-    let self = this
+  on(eventName, handler) {
+    let self = this;
     if (!this.handlers) {
-      this.handlers = {}
+      this.handlers = {};
     }
-    
+
     let handlers = this.handlers[eventName];
-    
+
     if (!handlers) {
-      handlers = this.handlers[eventName] = []
+      handlers = this.handlers[eventName] = [];
     }
-    
-    handlers.push(handler)
-    
+
+    handlers.push(handler);
+
     // Return an event descriptor
     return {
       name: eventName,
       callback: handler,
       un: (e, fn) => self.un(e, fn)
-    }
+    };
   }
-  
+
   /**
    * Remove an event handler.
    * @param eventName
    * @param handle
    */
-  un (eventName, handle) {
+  un(eventName, handle) {
     if (!this.handlers) {
-      return
+      return;
     }
-    
-    let handlers = this.handlers[eventName]
-    let i
-    
+
+    let handlers = this.handlers[eventName];
+    let i;
+
     if (handlers) {
       if (handle) {
-        for (i = handlers.length - 1 ; i >= 0 ; i--) {
-          handlers[i] == handle && handlers.splice(i, 1)
+        for (i = handlers.length - 1; i >= 0; i--) {
+          handlers[i] == handle && handlers.splice(i, 1);
         }
       } else {
-        handlers.length = 0
+        handlers.length = 0;
       }
     }
   }
-  
+
   /**
    * get user information from current sign in
    */
-  getCurrentUser () {
-    let userProfile = window.gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile()
-    return userProfile ? {
-      email: userProfile.getEmail(),
-      name: userProfile.getName(),
-      image: userProfile.getImageUrl(),
-      id: userProfile.getId(),
-      token: window.gapi.auth.getToken().access_token
-    } : {}
+  getCurrentUser() {
+    let userProfile = window.gapi.auth2
+      .getAuthInstance()
+      .currentUser.get()
+      .getBasicProfile();
+    return userProfile
+      ? {
+          email: userProfile.getEmail(),
+          name: userProfile.getName(),
+          image: userProfile.getImageUrl(),
+          id: userProfile.getId(),
+          token: window.gapi.auth.getToken().access_token
+        }
+      : {};
   }
-  
+
   /**
    * load google sdk
    * @private
    */
-  _loadGApi () {
+  _loadGApi() {
     return new Promise(async res => {
-      await this._loadScript('https://apis.google.com/js/api.js')
-      window.gapi.load('client:auth2', () => {
-        window.gapi.client.load('drive', 'v2', async () => {
+      await this._loadScript("https://apis.google.com/js/api.js");
+      window.gapi.load("client:auth2", () => {
+        window.gapi.client.load("drive", "v2", async () => {
           await window.gapi.client.init({
             apiKey: this.options.googleAPI.apiKey,
             clientId: this.options.googleAPI.clientId,
             discoveryDocs: this.options.discovery_docs,
             scope: this.options.scope
-          })
-          res()
-        })
-      })
-    })
+          });
+          res();
+        });
+      });
+    });
   }
-  
+
   /**
    * same as $.loadScript()
    * @param src
    * @returns {Promise<any>}
    * @private
    */
-  _loadScript (src) {
+  _loadScript(src) {
     return new Promise((res, rej) => {
-      let script = document.createElement('script')
-      script.async = true
-      script.onerror = () => rej()
-      script.onload = () => res(src)
-      script.src = src
-      document.querySelector('body').appendChild(script)
-    })
+      let script = document.createElement("script");
+      script.async = true;
+      script.onerror = () => rej();
+      script.onload = () => res(src);
+      script.src = src;
+      document.querySelector("body").appendChild(script);
+    });
   }
-  
 }
+
+export default DriveTreeCreator
